9f228bb061f2a88d2df8651b10546fdfd0691b21
anchal66 <anchalavinash66@gmail.com>
2022-01-30 21:49:42 +0530

    set value of state from dispatch

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,31 @@
-import { useSelector } from 'react-redux';
-import classes from './Counter.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+
   //Get peice of data from store and also subscribe for changes
-  const counter = useSelector(state => state.counter);
+  const counter = useSelector((state) => state.counter);
+
+  const dispatch = useDispatch();
+
+  const increseHandler = () =>{
+    dispatch({type: 'increment'});
+  }
+
+  const decreseHandler = () =>{
+    dispatch({type: 'decrement'});
+  }
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={increseHandler}>+</button>
+        <button onClick={decreseHandler}>-</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
