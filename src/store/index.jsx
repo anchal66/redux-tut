3ff9545a7dc8a7c2ccb8ad43646930d0671f6483
anchal66 <anchalavinash66@gmail.com>
2022-01-31 13:05:47 +0530

    Sepreating slices to different files

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,49 +1,9 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
-
-const initialCounterState = {
-  counter: 0,
-  showCounter: true,
-};
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: initialCounterState,
-  reducers: {
-    increment(state){
-      state.counter++;
-    },
-    decrement(state){
-      state.counter--;
-    },
-    increseBy5(state, action){
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state){
-      state.showCounter = !state.showCounter
-    }
-  }
-});
-
-const initialAuthState = {
-  isAuthentiated: false
-}
-const authSlice = createSlice({
-  name: "auth",
-  initialState: initialAuthState,
-  reducers:{
-    login(state){
-      state.isAuthentiated = true
-    },
-    logout(state){
-      state.isAuthentiated = false
-    }
-  }
-})
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./authSlice";
+import counterReducer from "./counterSlice";
 
 const store = configureStore({
-  reducer: {counter: counterSlice.reducer, auth: authSlice.reducer}
+  reducer: {counter: counterReducer, auth: authReducer}
 });
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
